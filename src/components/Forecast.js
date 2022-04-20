debd1feb8d13087b5ee17340bf1a1e758f04c906
Steve Clair <sclair@bird-technologies.com>
2022-04-20 13:49:26 -0400

    Sucessfully fetching required data

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,20 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+
 
 const Forecast = () => {
     const [data, setData] = useState({})
 
     useEffect(()=>{
-        axios
-            .get('https://api.openweathermap.org/data/2.5/weather?lat=41.505493&lon=-81.681290&appid=ddda86334bf110d91053b9bccca10443')
-                .then(res=>{
-                    setData(res.data)
-                    console.log(res.data)
-                })
-                .catch(console.log)
+        const fetchData = async()=>{
+            //Store this data in a .env later
+            await fetch('https://api.openweathermap.org/data/2.5/weather?lat=41.505493&lon=-81.681290&appid=ddda86334bf110d91053b9bccca10443')
+            .then(res => res.json())
+            .then(result =>{
+                setData(result)
+                console.log(result)
+            })
+        }
+        fetchData()
     }, [])
   return (
-    <div>Forecast</div>
+    <div>
+        {(typeof  data.main != 'undefined')?(
+            <div>
+                <h3>{data.name}</h3>
+                <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt='weather icon' />
+                <p>Temp: {Math.round(1.8*(data.main.temp - 273)+32)} &deg;F</p>
+                <p>Hi: {Math.round(1.8*(data.main.temp_max - 273)+32)} &deg;F</p>
+                <p>Lo: {Math.round(1.8*(data.main.temp_min - 273)+32)} &deg;F</p>
+            </div>
+            
+        ):(<div></div>)}
+        
+    </div>
   )
 }
 
