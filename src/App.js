ba2bf8f2e10577398885cf7d81fd92284d55cfb0
Steve Clair <sclair@bird-technologies.com>
2022-04-20 14:46:25 -0400

    card component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Forecast from './components/Forecast';
+import WeatherCard from './components/WeatherCard';
 
 function App() {
   return (
     <div className="App">
       <Forecast />
+      <WeatherCard />
     </div>
   );
 }
---
+++ src/components/WeatherCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../App.css"
+
+const WeatherCard = () => {
+  return (
+    <div className='card'>
+        <p className='card-title'>Weather Card</p>
+        <img className='card-image' src='' alt='' />
+        <div className='card-content'>
+        <p>Fugiat excepteur voluptate tempor sint ea. Aliquip do officia officia consequat ut.</p> 
+        </div>
+    </div>
+  )
+}
+
+export default WeatherCard
