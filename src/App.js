b51f159bab4e1747f76b156655d27f7fc17a2c7f
Steve Clair <sclair@bird-technologies.com>
2022-04-21 08:49:55 -0400

    fetch moved to app component.  weatherCard component created, forecast component removed. Part II complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { useEffect, useState} from 'react'
 import './App.css';
-import Forecast from './components/Forecast';
 import WeatherCard from './components/WeatherCard';
 
 function App() {
+  const [data, setData] = useState({})
+
+    useEffect(()=>{
+        const fetchData = async()=>{
+            //Store this data in a .env later
+            await fetch('https://api.openweathermap.org/data/2.5/weather?lat=41.505493&lon=-81.681290&appid=ddda86334bf110d91053b9bccca10443')
+            .then(res => res.json())
+            .then(result =>{
+                setData(result)
+            })
+        }
+        fetchData()
+    }, [])
   return (
     <div className="App">
-      <Forecast />
-      <WeatherCard />
+      <WeatherCard weatherData={data} />
     </div>
   );
 }
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import moment from 'moment';
 import "../App.css"
 
-const WeatherCard = () => {
+const WeatherCard = ({weatherData}) => {
   return (
-    <div className='card'>
-        <p className='card-title'>Weather Card</p>
-        <img className='card-image' src='' alt='' />
-        <div className='card-content'>
-        <p>Fugiat excepteur voluptate tempor sint ea. Aliquip do officia officia consequat ut.</p> 
+    <div>
+      {(typeof  weatherData.main != 'undefined')?(
+            <div className='card'>
+            <h3 className='card-title'>{moment().format('ddd')}</h3>
+            <img className='card-img' src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt='weather icon' />
+            <div className='card-content'>
+            <p>{Math.round(1.8*(weatherData.main.temp_min - 273)+32)} &deg;</p>
+            <p>{Math.round(1.8*(weatherData.main.temp_max - 273)+32)} &deg;</p>
+            </div>
         </div>
+            
+        ):(<div></div>)}
     </div>
+    
   )
 }
 
