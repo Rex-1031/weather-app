dc8fdeb7d30853bdd19306e75b327208a9cee545
Steve Clair <sclair@bird-technologies.com>
2023-02-13 18:59:11 -0500

    changed a link

--- serverless/get_weather.js
+++ serverless/get_weather.js
@@ -5,7 +5,7 @@ const { WEATHER_API_KEY } = process.env;
 exports.handler = async (event, context) => {
   const params = JSON.parse(event.body);
   const { lat, lon, units } = params;
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
+  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
 
   try {
     const weatherStream = await fetch(url);
